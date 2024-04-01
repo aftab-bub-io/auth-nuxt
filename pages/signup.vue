cef42aa95d2526dc98de395ba754ba281f3a38c4
aftab-bub-io <aftab.500apps@gmail.com>
2024-04-01 17:40:14 +0530

    signup integrated

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,37 +1,67 @@
 <script setup>
 import { ref } from 'vue'
+const supabase = useSupabaseClient()
+
+
 const submitted = ref(false)
 const submitHandler = async (formData) => {
-  console.log(formData)
+  const { data, error } = await supabase.auth.signUp({
+    name: formData.name,
+    email: formData.email,
+    password: formData.password,
+    options: {
+      data: {
+        first_name: formData.name,
+      }
+    }
+  })
+  console.log(data);
+}
+
+
+const handleIconClick = (node, e) => {
+  node.props.suffixIcon = node.props.suffixIcon === 'eye' ? 'eyeClosed' : 'eye'
+  node.props.type = node.props.type === 'password' ? 'text' : 'password'
 }
 </script>
 
 <template>
-  <FormKit type="form" id="registration-example" :form-class="submitted ? 'hide' : 'show'" submit-label="Register"
-    @submit="submitHandler" :actions="false" #default="{ value }">
-    <h1 class="text-2xl font-bold mb-2">Register!</h1>
-    <p class="text-sm mb-4">
-      You can put any type of element inside a form, not just FormKit inputs
-      (although only FormKit inputs are included with the submission).
-    </p>
-    <FormKit type="text" name="name" label="Your name" placeholder="Jane Doe" help="What do people call you?"
-      validation="required" />
-    <FormKit type="text" name="email" label="Your email" placeholder="jane@example.com" help="What email should we use?"
-      validation="required|email" />
-    <div class="double">
-      <FormKit type="password" name="password" label="Password" validation="required|length:6|matches:/[^a-zA-Z]/"
-        :validation-messages="{
-    matches: 'Please include at least one symbol',
-  }" placeholder="Your password" help="Choose a password" prefix-icon="password" suffix-icon="eyeClosed"
-        @suffix-icon-click="handleIconClick" suffix-icon-class="hover:text-blue-500" />
-      <FormKit type="password" name="password_confirm" label="Confirm password" placeholder="Confirm password"
-        validation="required|confirm" help="Confirm your password" />
-    </div>
+  <div class="w-80 m-auto h-screen flex items-center justify-center">
+    <FormKit type="form" id="registration-example" :form-class="submitted ? 'hide' : 'show'" submit-label="Register"
+      @submit="submitHandler" :actions="false" #default="{ value }">
+      <h1 class="text-2xl font-bold mb-2 text-center uppercase text-slate-800">Sign Up</h1>
+      <FormKit type="text" name="name" label="Your name" placeholder="Jane Doe" help="What do people call you?"
+        validation="required" />
+      <FormKit type="text" name="email" label="Your email" placeholder="jane@example.com"
+        help="What email should we use?" validation="required|email" />
+      <div class="double">
+        <FormKit type="password" name="password" label="Password" validation="required|length:6|matches:/[^a-zA-Z]/"
+          :validation-messages="{
+      matches: 'Please include at least one symbol',
+    }" placeholder="Your password" help="Choose a password" prefix-icon="password" suffix-icon="eyeClosed"
+          @suffix-icon-click="handleIconClick" suffix-icon-class="hover:text-blue-500" />
+        <FormKit type="password" name="password_confirm" label="Confirm password" placeholder="Confirm password"
+          validation="required|confirm" help="Confirm your password" prefix-icon="password" suffix-icon="eyeClosed"
+          @suffix-icon-click="handleIconClick" suffix-icon-class="hover:text-blue-500" />
+      </div>
 
-    <FormKit type="submit" label="Register" />
-    <pre wrap>{{ value }}</pre>
-  </FormKit>
-  <div v-if="submitted">
-    <h2 class="text-xl text-green-500">Submission successful!</h2>
+      <FormKit type="submit" label="Register" :classes="{ outer: { 'my-button': true }, input: { $reset: true } }" />
+      <!-- <pre wrap>{{ value }}</pre> -->
+    </FormKit>
+    <div v-if="submitted">
+      <h2 class="text-xl text-green-500">Submission successful!</h2>
+    </div>
   </div>
 </template>
+
+<style>
+.my-button {
+  background-color: rgb(47, 75, 167);
+  border-radius: 5px;
+  text-align: center;
+  padding: 5px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+}
+</style>
